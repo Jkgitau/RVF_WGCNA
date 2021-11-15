f6e35dffc1075a8e52e33c868df8400acc2088c1
jkgitau <johngitau51@gmail.com>
2021-11-15 17:11:58 +0300

    settings.R deleted

--- scripts/settings.R
+++
@@ -1,9 +0,0 @@
-# ensure results are reproducible
-set.seed(1)
-
-# other settings
-options(digits = 4)
-options(stringsAsFactors = FALSE)
-
-# set number of threads to allow in WGCNA analysis
-allowWGCNAThreads(nThreads=20)
